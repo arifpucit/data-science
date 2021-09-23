277fa5a07ad226b12ee86887c6cc925c088e0cb8
Muhammad Arif Butt <arif@pucit.edu.pk>
2021-09-23 18:52:15 +0500

    Add files via upload

---
+++ lec-2.6/package files/packageDemo/package2/comparison.py
@@ -0,0 +1,12 @@
+def comparison(x,y):
+    print("Comparing both operand where x is ",x,"and y is ",y)
+    
+    if(x>y):
+        print(" x > y is: ",  x>y)
+        
+    elif(x<y):
+        print(" x < y is: ", x<y)
+        
+    else:
+        print(" x == y is: ", x==y)
+    
